743c2224653f2fff6441af72857a9f2a0a9a8ceb
Michaela Davis <michaela.delight@gmail.com>
2022-06-09 15:18:52 -0700

    create mock heroines

---
+++ src/app/mock-heroines.ts
@@ -0,0 +1,13 @@
+import { Heroine } from './heroine';
+
+export const HEROINES: Heroine[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberWoman' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Ms. Magma' },
+  { id: 20, name: 'Tornado' }
+];
