f385031c39a54336f8786fe44f77fb0a42f6ccbf
Michaela Davis <michaela.delight@gmail.com>
2022-06-10 08:23:27 -0700

    show messages

--- src/app/heroine.service.ts
+++ src/app/heroine.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Heroine } from './heroine';
 import { HEROINES } from './mock-heroines';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 // You must make the HeroService available to the dependency injection system before Angular can inject it into the HeroesComponent by registering a provider. A provider is something that can create or deliver a service; in this case, it instantiates the HeroService class to provide the service.
 
@@ -14,8 +15,10 @@ export class HeroineService {
 
   getHeroines(): Observable<Heroine[]> {
     const heroines = of(HEROINES);
+    this.messageService.add('HeroineService: fetched heroines');
     return heroines;
   }
 
-  constructor() { }
+  // This is a typical "service-in-service" scenario: You inject the MessageService into the HeroService which is injected into the HeroesComponent.
+  constructor(private messageService: MessageService) { }
 }
