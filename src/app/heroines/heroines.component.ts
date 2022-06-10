eb230881e7dddb4aa92511e162722720e5b94d2b
Michaela Davis <michaela.delight@gmail.com>
2022-06-09 19:40:11 -0700

    update HeroinesComponent (but there are errors)

--- src/app/heroines/heroines.component.ts
+++ src/app/heroines/heroines.component.ts
@@ -1,7 +1,8 @@
 // You always import the Component symbol from the Angular core library and annotate the component class with @Component.
 import { Component, OnInit } from '@angular/core';
 import { Heroine } from '../heroine';
-import { HEROINES } from '../mock-heroines';
+import { HeroineService } from '../heroine.service';
+
 
 // @Component is a decorator function that specifies the Angular metadata for the component.
 @Component({
@@ -11,22 +12,23 @@ import { HEROINES } from '../mock-heroines';
 })
 export class HeroinesComponent implements OnInit {
 
-  heroines = HEROINES;
+  heroines = Heroine[] =[];
 
-  heroine: Heroine = {
-    id: 1,
-    name: 'Captain Marvel'
-  };
   // assigns the clicked hero from the template to the component's selectedHeroine
   selectedHeroine?: Heroine;
   onSelect(heroine: Heroine): void {
     this.selectedHeroine = heroine;
-}
+  }
 
-  constructor() { }
+  constructor(private heroineService: HeroineService) {}
 
   // The ngOnInit() is a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic.
   ngOnInit(): void {
+    this.getHeroines();
+  }
+
+  getHeroines(): void {
+    this.heroines = this.heroineService.getHeroines();
   }
 
 }
