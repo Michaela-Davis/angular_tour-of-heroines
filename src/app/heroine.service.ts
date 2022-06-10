444f9c3d94de6d606c899ecca13c4d88613a89f8
Michaela Davis <michaela.delight@gmail.com>
2022-06-09 19:12:52 -0700

    create & provide the HeroineService 🤣

---
+++ src/app/heroine.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Heroine } from './heroine';
+import { HEROINES } from './mock-heroines';
+
+// You must make the HeroService available to the dependency injection system before Angular can inject it into the HeroesComponent by registering a provider. A provider is something that can create or deliver a service; in this case, it instantiates the HeroService class to provide the service.
+
+// To make sure that the HeroService can provide this service, register it with the injector, which is the object that is responsible for choosing and injecting the provider where the application requires it.
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HeroineService {
+
+  getHeroines(): Heroine[] {
+    return HEROINES;
+  }
+
+  constructor() { }
+}
